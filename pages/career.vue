f8b4ae54e25239dc5f7ad99ed45ae2ee086b97ac
aepsaefulloh <aepsaefulloh1396@gmail.com>
2025-12-15 13:01:34 +0700

    Update career page with new layout and contact email

--- pages/career.vue
+++ pages/career.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
+        <section class="bg-idealive text-white py-20 md:py-32 px-desktop h-dvh relative ">
+            <div class="w-full h-full flex text-center items-center justify-center">
+                <h2 class="mt-12 md:mt-18 text-3xl md:text-5xl lg:text-[82px] !leading-[1.3]">
+                    Join Our Team at Idealive
+                </h2>
+            </div>
+            <div class="w-full text-center absolute bottom-12 md:bottom-10 left-0 px-desktop">
+                <div class="mt-12 md:mt-18 lg:text-xl !leading-[1.3] text-gray-300 mb-3">
+                    Apply now!
+                </div>
+                <div class="">
 
+                    <a href="mailto:careers@idealive.com"
+                        class="text-xl md:text-2xl lg:text-4xl inline-flex items-center justify-center gap-3 font-medium hover:opacity-80 transition-opacity">
+                        <svg class="w-[0.6em] h-[0.6em] mt-[0.1em]" viewBox="0 0 12 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 6L0 12L0 0L12 6Z" fill="currentColor" />
+                        </svg>
+                        career@idealive.com
+                    </a>
+                </div>
+
+            </div>
+        </section>
     </div>
 </template>
 
@@ -8,6 +31,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
